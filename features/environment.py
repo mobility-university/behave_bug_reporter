9807d0b9853727edafae9db0531de2167cced86a
Philip <44866668+px-d@users.noreply.github.com>
2023-02-03 13:50:39 +0100

    Bugreport Automation

--- features/environment.py
+++ features/environment.py
@@ -1,29 +1,49 @@
 from behave.model_core import Status
-from itertools import chain
+from features.bug import BugReport, report_bug
+import traceback
+import os
+
+bug_reports = []
+
+
+def before_scenario(context, scenario):
+    assert "exception" not in dir(context)
+    context.exception = None
+
+
+def before_step(context, step):
+    callback = step.store_exception_context
+
+    def store_exception_context(exception):
+        context.exception = dict(exception=exception, traceback=traceback.format_exc())
+        callback(exception)
+
+    step.store_exception_context = store_exception_context
 
 
 def after_scenario(context, scenario):
     if scenario.status != Status.failed:
         return
-    
-    with open('bug.txt', 'w') as file:
-        file.write(f'Scenario: {scenario.name} failed\n\n')
-        file.write('Steps:\n')
-        for step in chain(scenario._background_steps or [], scenario.steps):
-            file.write(f'  {step.step_type} {step.name}\n')
-            if step.table:
-                assert False, 'hey, implement me please'
-            if step.text:
-                file.write(f'"""\n{step.text}\n"""\n')
-        if context.stdout_capture:
-            file.write("\n\n")
-            file.write('stdout:\n')
-            file.write(context.stdout_capture.getvalue())
-        if context.stderr_capture:
-            file.write("\n\n")
-            file.write('stderr:\n')
-            file.write(context.stderr_capture.getvalue())
-        if scenario.error_message:
-            file.write("\n\n")
-            file.write('error:\n')
-            file.write(scenario.error_message.getvalue())
+
+    # Report Bug!
+    error_type = type(context.exception["exception"])
+    error_descr = context.exception["traceback"]
+    bug_reports.append(
+        BugReport(
+            scenario,
+            error_type,
+            error_descr,
+            versions={"youtube": "7.8.2", "github": "1.2.3"},
+        )
+    )
+
+
+def after_all(context):
+    if os.environ.get("CREATE_BUG_REPORT", "False") == "True":
+        print("Creating Bug Reports...")
+        for r in bug_reports:
+            print(r.prettify())
+            print()
+            print(r.repro_steps())
+            print(r.get_versions())
+            # report_bug(r)
